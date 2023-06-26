11976065afebed128cbe7cdb023892e4025317a9
vgoncalves <djvic123@hotmail.com>
2023-06-26 18:38:29 +0100

    implement get from the WS

--- app/src/main/java/com/vic/publications2/MainActivity.kt
+++ app/src/main/java/com/vic/publications2/MainActivity.kt
@@ -2,10 +2,42 @@ package com.vic.publications2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+const val BASE_URL = "https://beira.pt"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        makeAPIRequest()
+    }
+
+    private fun makeAPIRequest() {
+
+        val api = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(APIRequest::class.java)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val response = api.getNews()
+
+                for (article in response) {
+                    Log.i("MainActivity", "Result = $article")
+                }
+            } catch (e: Exception) {
+                Log.i("MainActivity", e.toString())
+            }
+
+        }
     }
 }
